09647d9e2d182b17832210966ce0a04e84760db6
YungPharaoh65 <princemsimango27@gmail.com>
2024-09-05 20:09:14 +0200

    made changes on pages

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RouterLink, RouterOutlet],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
