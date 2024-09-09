f52039fa1ba4ec7b6a39e5dc1c6395f1d9d5f347
YungPharaoh65 <princemsimango27@gmail.com>
2024-09-09 21:55:16 +0200

    made changes

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet , Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
@@ -10,5 +10,27 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+
+  
+  constructor(private router: Router) {}
+ 
+
+  isDesktop: boolean = false; // Default value set to false
+
+  ngOnInit(): void {
+    this.checkDevice(); // Check device on initialization
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkDevice(); // Check device on window resize
+  }
+
+  checkDevice() {
+    const width = window.innerWidth;
+    this.isDesktop = width >= 1025;
+    console.log(`Screen width: ${width}, Is Desktop: ${this.isDesktop}`);
+  }
+
 
 }
