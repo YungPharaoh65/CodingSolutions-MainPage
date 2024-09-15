525a35ea7c52a6aa6b94708e419ed81de35c3cc5
YungPharaoh65 <princemsimango27@gmail.com>
2024-09-15 16:01:18 +0200

    made changes

--- src/app/moredetails/moredetails.component.ts
+++ src/app/moredetails/moredetails.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faInbox, faCode } from '@fortawesome/free-solid-svg-icons';
 import {
@@ -42,5 +42,34 @@ export class MoredetailsComponent {
   faJava = faJava;
 
   faCode = faCode;
+  loading: boolean | undefined;
+
+  constructor(private router: Router) {}
+  isDesktop: boolean = false; // Default value set to false
+
+  ngOnInit(): void {
+    this.checkDevice(); // Check device on initialization
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkDevice(); // Check device on window resize
+  }
+
+  checkDevice() {
+    const width = window.innerWidth;
+    this.isDesktop = width >= 1025;
+    console.log(`Screen width: ${width}, Is Desktop: ${this.isDesktop}`);
+  }
+
+  onButtonClick() {
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+      this.router.navigate(['/pricing']);
+    }, 3000);
+  }
+
+
 
 }
